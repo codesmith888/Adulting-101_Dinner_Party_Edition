6308a50527f243b216c332c2297e6546f3b4b965
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-07-06 21:54:31 -0700

    changed my routes and views to use fewer still working on saving and rendering the menu page

--- controllers/adulting101.js
+++ controllers/adulting101.js
@@ -17,31 +17,62 @@ router.get('/newAppetizer', ((req, res) => {
     foundUser.addMenu(newMenu)
     console.log(`You created a new menu called ${newMenu.name}`)
   }).then(relationInfo => {
-    res.render(`adulting101/newAppetizer`)
+    res.render('adulting101/newMenu')
   }).catch((error) => 
   console.log(error)
   )
   })
 }))
 
-router.post('/newAppetizer', ((req, res) => {
-  let recipeUrl = `http://www.recipepuppy.com/api/?q=appetizer&i=${req.body.app_ingredients}`
-  axios.get(recipeUrl).then((apiResponse) => {
+router.post('/newMenu', ((req, res) => {
+  let appetizerUrl = `http://www.recipepuppy.com/api/?q=appetizer&i=${req.body.app_ingredients}`
+  axios.get(appetizerUrl).then((apiResponse) => {
     let appetizerOptions = apiResponse.data
-    res.render('adulting101/appetizer', {appetizer: appetizerOptions})
-  }).catch((error) => 
-  console.log(error)
-  )
-  }))
+  let mainUrl = `http://www.recipepuppy.com/api/?&i=${req.body.main_ingredients}`
+    axios.get(mainUrl).then((apiResponse) => {
+    let mainOptions = apiResponse.data
+  let sideUrl =  `http://www.recipepuppy.com/api/?q=side&i=${req.body.side_ingredients}`
+    axios.get(sideUrl).then((apiResponse) => {
+      let sideOptions = apiResponse.data
+  let dessertUrl = `http://www.recipepuppy.com/api/?q=dessert&i=${req.body.dessert_ingredients}`
+    axios.get(dessertUrl).then((apiResponse) => {
+      let dessertOptions = apiResponse.data
+  res.render('adulting101/choose', {dessert: dessertOptions, appetizer: appetizerOptions, main: mainOptions, side: sideOptions})
+      }).catch((error) => {
+        console.log(error)
+        })
+      })
+    })
+  })
+}));
+
 
 
 router.post('/menu', ((req, res) => {
-  let chosenAppetizer = req.body
-  res.render('./adulting101/menu', {appetizer: chosenAppetizer})
+  let chosenAppetizer = req.body.appetizer
+  let chosenMain = req.body.mainCourse
+  let chosenSide = req.body.sideDish
+  let chosenDessert = req.body.dessert
+  console.log(chosenAppetizer)
+  res.render('./adulting101/menu', {appetizer: chosenAppetizer, main: chosenMain, side: chosenSide, dessert: chosenDessert})
 }))
 
-router.post('/newMain', ((req, res) => {
-  res.render('./adulting101/newMain')
+router.get('/newMenu', ((req, res) => {
+  res.render('./adulting101/newMenu')
 }))
+
+// router.get('/newMain', ((req, res) => {
+//   res.render('./adulting101/newMain')
+// }))
+
+// router.post('/newMain', ((req, res) => {
+//   let recipeUrl = `http://www.recipepuppy.com/api/?i=${req.body.main_ingredients}`
+//   axios.get(recipeUrl).then((apiResponse) => {
+//     let mainCourseOptions = apiResponse.data
+//     res.render('adulting101/maincourse', {mainCourse: mainCourseOptions})
+//   }).catch((error) => 
+//   console.log(error)
+//   )
+//   }))
 
 module.exports = router;
